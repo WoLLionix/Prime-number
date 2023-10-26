9d7e3d49b4da66667545948900ff4001dd8950dc
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-26 16:13:24 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+bool primeNumber(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+            count++;
+    }
+    if (count == 2)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int num;
+    std::cout << "Enter the number:" << std::endl;
+    std::cin >> num;
+
+    if (num < 1)
+    {
+        std::cout << "The prime number can`t be less than 1";
+        return 0;
+    }
+
+    std::cout << "The number is prime ?" << std::endl;
+
+    if (primeNumber(num))
+        std::cout << "Yes";
+    else
+        std::cout << "No";
+}
